b36e84e41acf3ea69ccac2aa93d9988cac19663d
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2024-12-11 21:03:30 +0300

    rename classes, add lesson4

--- com/tms/lesson4/Task2.java
+++ com/tms/lesson4/Task2.java
@@ -1,9 +1,11 @@
-package com.tms;
+package com.tms.lesson4;
 
-public class HomeTask_4_2 {
+public class Task2 {
+
     public static void main(String[] args) {
 
         int[] mass = {13, 67, 44, 9, 56, 20};
+
         int max = mass[0];
         int min = mass[0];
         int sum = 0;
@@ -21,10 +23,7 @@ public class HomeTask_4_2 {
         }
         System.out.println("Количество элементов в массиве: " + mass.length);
         System.out.println("Сумма всех элементов в массиве: " + sum);
-        System.out.println("Максимальная сумма всех элементов в массиве: " + max);
-        System.out.println("Минимальная сумма всех элементов в массиве: " + min);
+        System.out.println("Максимальный элемент в массиве: " + max);
+        System.out.println("Минимальный элемент в массиве: " + min);
     }
-
 }
-
-
