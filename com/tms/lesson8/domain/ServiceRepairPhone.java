27d00d98d14b4834e2b93e750223404506c19686
Alesya Yaroshenko <alesya.yaroshenko@betera.by>
2024-12-25 14:55:36 +0300

    fixed

--- com/tms/lesson8/domain/ServiceRepairPhone.java
+++ com/tms/lesson8/domain/ServiceRepairPhone.java
@@ -2,14 +2,12 @@ package com.tms.lesson8.domain;
 
 public class ServiceRepairPhone {
 
-    public void check(Object o){
-        if(o instanceof Phone) {
-            Phone val = (Phone) o;
-        val.call(val.phoneNumber);
-        val.play(val.levelVolume);
-        val.firmware(val.getVersion());
+    public void check(Phone phone){
+        phone.call(phone.phoneNumber);
+        phone.play(phone.levelVolume);
+        phone.firmware(phone.getVersion());
     }
 }
 
 
-}
+
